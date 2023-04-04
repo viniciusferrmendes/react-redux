8b1c87739ec8ca54e5df405db4ff54c2c99d3e76
unknown <vinicius.ferrmendes@gmail.com>
2023-04-04 18:56:33 +0000

    using react fragment

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom";
 
 import First from "./components/basics/First";
 import WithParameter from "./components/basics/WithParameter";
+import Fragment from "./components/basics/Fragment";
 
 import "./index.css";
 
 ReactDOM.render(
-    <div>
+    <div id="app">
         <First />
         <WithParameter course="React/Redux" student="Vinicius" grade={6.7} />
+        <Fragment />
     </div>,
     document.getElementById("root")
 );

68def8e482e9b5aff3daed61cee339fa64aae226
unknown <vinicius.ferrmendes@gmail.com>
2023-04-04 18:39:50 +0000

    using component parameters - props

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import First from "./components/basics/First";
+import WithParameter from "./components/basics/WithParameter";
 
 import "./index.css";
 
 ReactDOM.render(
     <div>
-        <First></First>
+        <First />
+        <WithParameter course="React/Redux" student="Vinicius" grade={4} />
     </div>,
     document.getElementById("root")
 );

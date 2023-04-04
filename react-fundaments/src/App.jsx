6bea1d94ee29386507145ca85f945710c3de4fa6
unknown <vinicius.ferrmendes@gmail.com>
2023-04-04 23:56:41 +0000

    using flexbox

--- react-fundaments/src/App.jsx
+++ react-fundaments/src/App.jsx
@@ -6,26 +6,30 @@ import Fragment from "./components/basics/Fragment";
 import Random from "./components/basics/Random";
 import Card from "./components/Layout/Card/Card";
 
+import "./App.css";
+
 export default () => {
     return (
-        <div id="app">
+        <div className="App">
             <h1>React Fundaments</h1>
 
-            <Card title="#04 - Random Challenge">
-                <Random max={100} min={0} />
-            </Card>
+            <div className="Cards">
+                <Card title="#04 - Random Challenge">
+                    <Random max={100} min={0} />
+                </Card>
 
-            <Card title="#03 - Fragment">
-                <Fragment />
-            </Card>
+                <Card title="#03 - Fragment">
+                    <Fragment />
+                </Card>
 
-            <Card title="#02 - With Parameter">
-                <WithParameter course="React/Redux" student="Vinicius" grade={6.7} />
-            </Card>
+                <Card title="#02 - With Parameter">
+                    <WithParameter course="React/Redux" student="Vinicius" grade={6.7} />
+                </Card>
 
-            <Card title="#01 - First">
-                <First />
-            </Card>
+                <Card title="#01 - First">
+                    <First />
+                </Card>
+            </div>
         </div>
     );
 }

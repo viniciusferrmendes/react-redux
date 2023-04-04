5d1af8ea2b13b080ed7e6cecf5de03ec6616ea16
unknown <vinicius.ferrmendes@gmail.com>
2023-04-04 22:38:51 +0000

    App component

--- react-fundaments/src/App.jsx
+++ react-fundaments/src/App.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 import First from "./components/basics/First";
 import WithParameter from "./components/basics/WithParameter";
 import Fragment from "./components/basics/Fragment";
 
-import "./index.css";
-
-ReactDOM.render(
+export default _ =>
     <div id="app">
-        <First />
-        <WithParameter course="React/Redux" student="Vinicius" grade={6.7} />
+        <h1>React Fundaments</h1>
         <Fragment />
-    </div>,
-    document.getElementById("root")
-);
+        <WithParameter course="React/Redux" student="Vinicius" grade={6.7} />
+        <First />
+    </div>

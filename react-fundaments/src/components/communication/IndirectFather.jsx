013a58bf2c3feefc743fb1cae7d256e50fd455a2
unknown <vinicius.ferrmendes@gmail.com>
2023-04-09 00:28:54 +0000

    component with state - useState

--- react-fundaments/src/components/communication/IndirectFather.jsx
+++ react-fundaments/src/components/communication/IndirectFather.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import IndirectSon from "./IndirectSon";
 
 export default props => {
+    const [name, setName] = useState("?");
+    const [age, setAge] = useState(0);
+    const [dev, setDev] = useState(false);
 
     function provideInfos(name, age, dev) {
-        console.log(name, age, dev);
+        setName(name);
+        setAge(age);
+        setDev(dev);
     }
 
     return (
         <div>
-            <div>Pai</div>
+            <span>{name} </span>
+            <span><strong>{age}</strong> </span>
+            <span>{dev ? "TRUE" : "FALSE"}</span>
             <IndirectSon callback={provideInfos} />
         </div>
     );
--- react-fundaments/src/components/communication/IndirectSon.jsx
+++ react-fundaments/src/components/communication/IndirectSon.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 export default ({ callback }) => {
+    const min = 50;
+    const max = 70;
+    const randomAge = () => (Math.random() * (max - min) + min).toFixed();
+    const randomDev = () => Math.random() > 0.5;
+
     return (
         <div>
             <div>Filho</div>
-            <button onClick={() => callback("Gilson", 58, false)}>
+            <button onClick={() => callback("Gilson", randomAge(), randomDev())}>
                 Provide Informations
             </button>
         </div>

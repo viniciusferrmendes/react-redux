4f551ee336260798db5ac94c5fc579c99294a343
unknown <vinicius.ferrmendes@gmail.com>
2023-05-17 17:02:21 +0000

    Calculator logic

--- calculator-project/src/main/Calculator.jsx
+++ calculator-project/src/main/Calculator.jsx
@@ -1,46 +1,102 @@
-import React, { useState } from "react";
-
-import Display from "../components/Display/Display";
-import Button from "../components/Button/Button";
-
+import React, { Component } from "react";
 import "./Calculator.css";
 
-export default props => {
+import Button from "../components/Button/Button";
+import Display from "../components/Display/Display";
 
-    const [value, setValue] = useState(0);
+const initialState = {
+    displayValue: "0",
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+};
 
-    function clearMemory() {
-        setValue(0);
+export default class Calculator extends Component {
+
+    state = { ...initialState };
+
+    constructor(props) {
+        super(props);
+
+        this.clearMemory = this.clearMemory.bind(this);
+        this.setOperation = this.setOperation.bind(this);
+        this.addDigit = this.addDigit.bind(this);
     }
 
-    function setOperation(operation) {
-        console.log(operation);
+    clearMemory() {
+        this.setState({ ...initialState });
     }
 
-    function addDigit(digit) {
-        console.log(digit);
+    setOperation(operation) {
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true });
+        } else {
+            const equals = operation === "=";
+            const currentOperation = this.state.operation;
+
+            const values = [...this.state.values];
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`).toString();
+            } catch (e) {
+                values[0] = this.state.values[0];
+            }
+
+            values[1] = 0;
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            });
+        }
     }
 
-    return (
-        <div className="calculator">
-            <Display value={value} />
-            <Button label="AC" click={clearMemory} triple />
-            <Button label="/" click={setOperation} operation />
-            <Button label="7" click={addDigit} />
-            <Button label="8" click={addDigit} />
-            <Button label="9" click={addDigit} />
-            <Button label="*" click={setOperation} operation />
-            <Button label="4" click={addDigit} />
-            <Button label="5" click={addDigit} />
-            <Button label="6" click={addDigit} />
-            <Button label="-" click={setOperation} operation />
-            <Button label="1" click={addDigit} />
-            <Button label="2" click={addDigit} />
-            <Button label="3" click={addDigit} />
-            <Button label="+" click={setOperation} operation />
-            <Button label="0" click={addDigit} double />
-            <Button label="." />
-            <Button label="=" click={setOperation} operation />
-        </div>
-    );
+    addDigit(n) {
+        console.log(this.state.displayValue);
+        if (n === "." && this.state.displayValue.includes(".")) {
+            return;
+        }
+
+        const clearDisplay = this.state.displayValue === "0" || this.state.clearDisplay;
+        const currentValue = clearDisplay ? "" : this.state.displayValue;
+        const displayValue = currentValue + n;
+        this.setState({ displayValue, clearDisplay: false });
+
+        if (n !== ".") {
+            const i = this.state.current;
+            const newValue = parseFloat(displayValue);
+            const values = [...this.state.values];
+            values[i] = newValue;
+            this.setState({ values });
+            console.log(values);
+        }
+    }
+
+    render() {
+        return (
+            <div className="calculator">
+                <Display value={this.state.displayValue} />
+                <Button label="AC" click={this.clearMemory} triple />
+                <Button label="/" click={this.setOperation} operation />
+                <Button label="7" click={this.addDigit} />
+                <Button label="8" click={this.addDigit} />
+                <Button label="9" click={this.addDigit} />
+                <Button label="*" click={this.setOperation} operation />
+                <Button label="4" click={this.addDigit} />
+                <Button label="5" click={this.addDigit} />
+                <Button label="6" click={this.addDigit} />
+                <Button label="-" click={this.setOperation} operation />
+                <Button label="1" click={this.addDigit} />
+                <Button label="2" click={this.addDigit} />
+                <Button label="3" click={this.addDigit} />
+                <Button label="+" click={this.setOperation} operation />
+                <Button label="0" click={this.addDigit} double />
+                <Button label="." click={this.addDigit} />
+                <Button label="=" click={this.setOperation} operation />
+            </div>
+        );
+    }
 }

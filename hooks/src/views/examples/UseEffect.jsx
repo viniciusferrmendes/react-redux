42b992ec5a9de3456ea2b4fe1cb1e9c28c6abf26
unknown <vinicius.ferrmendes@gmail.com>
2023-05-18 12:04:19 +0000

    #02 - useEffect exercise

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -6,17 +6,26 @@ export default props => {
 
     const [number, setNumber] = useState(1);
     const [factorial, setFactorial] = useState(1);
+    const [status, setStatus] = useState("ODD");
 
     const calcFactorial = num => {
+        if (num === "") return "";
         const n = parseInt(num);
+
         if (n < 0) return -1;
         if (n === 0) return 1;
 
         return calcFactorial(n - 1) * n;
     }
 
+    const calcStatus = n => {
+        if (n === "") return "";
+        return n % 2 === 0 ? "EVEN" : "ODD";
+    }
+
     useEffect(() => {
         setFactorial(calcFactorial(number));
+        setStatus(calcStatus(number));
     }, [number]);
 
     return (
@@ -30,6 +39,14 @@ export default props => {
                     <span className="text red">{factorial !== -1 ? factorial : "Don't exist"}</span>
                 </div>
                 <input type="number" className="input" value={number} onChange={e => setNumber(e.target.value)} />
+            </div>
+
+            <SectionTitle title="Exercise #02" />
+            <div className="center">
+                <div>
+                    <span className="text">Status: </span>
+                    <span className="text red">{status}</span>
+                </div>
             </div>
         </div>
     );

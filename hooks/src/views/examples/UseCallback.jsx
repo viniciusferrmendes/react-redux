307cbae6fb6896a525ac2faff2ea7dc5b338a0b7
unknown <vinicius.ferrmendes@gmail.com>
2023-05-20 16:56:19 +0000

    useCallback

--- hooks/src/views/examples/UseCallback.jsx
+++ hooks/src/views/examples/UseCallback.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useCallback, useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import UseCallbackButtons from "./UseCallbackButtons";
 
-const UseCallback = (props) => {
+export default props => {
+
+    const [count, setCount] = useState(0);
+
+    const inc = useCallback(delta => setCount(curr => curr + delta), [setCount]);
+
     return (
         <div className="UseCallback">
-            <PageTitle
-                title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
-            />
-        </div>
-    )
-}
+            <PageTitle title="Hook UseCallback" subtitle="Retorna uma função memorizada!" />
 
-export default UseCallback
+            <div className="center">
+                <span className="text">{count}</span>
+                <UseCallbackButtons inc={inc} />
+            </div>
+        </div>
+    );
+}
---
+++ hooks/src/views/examples/UseCallbackButtons.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UseCallbackButtons = ({ inc }) => {
+    return (
+        <div>
+            <button className="btn" onClick={() => inc(6)}>+6</button>
+            <button className="btn" onClick={() => inc(12)}>+12</button>
+            <button className="btn" onClick={() => inc(18)}>+18</button>
+        </div>
+    );
+};
+
+export default React.memo(UseCallbackButtons);
